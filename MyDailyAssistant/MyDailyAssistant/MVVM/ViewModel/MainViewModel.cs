5ebdb2732f484d253c014287cca253b0636c1aa8
Bruno <brunoftlk@gmail.com>
2024-09-13 19:19:24 +0100

    initial commit

---
+++ MyDailyAssistant/MyDailyAssistant/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MyDailyAssistant.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDailyAssistant.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+    }
+}
